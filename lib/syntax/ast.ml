cdd3ce57ffd23a5b1e1001aa59779673c4c4fd8f
butterunderflow <azhong.934@gmail.com>
2023-10-03 06:42:17 +0800

    tweak syntax

--- lib/syntax/ast.ml
+++ lib/syntax/ast.ml
@@ -12,12 +12,15 @@ type pattern =
 type type_expr = TCons of string * type_expr list | TVar of string
 [@@deriving sexp]
 
+type para = PAnn of string * type_expr | PBare of string [@@deriving sexp]
+type paras = para list [@@deriving sexp]
+
 type expr =
   | EConst of constant
   | EVar of string
   | ELet of pattern * expr * expr
-  | ELetrec of (pattern * expr) list * expr
-  | ELam of pattern * expr
+  | ELetrec of (string * paras * expr) list * expr
+  | ELam of para * expr
   | EIf of expr * expr * expr
   | ECase of expr * (pattern * expr) list
   | EApp of expr * expr
@@ -25,8 +28,8 @@ type expr =
 [@@deriving sexp]
 
 type variant = string * type_expr list [@@deriving sexp]
-type para = PAnn of string * type_expr | PBare of string [@@deriving sexp]
-type paras = para list [@@deriving sexp]
+
+
 type type_paras = string list [@@deriving sexp]
 
 type top_level =
