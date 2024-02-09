97b511833e2295e420bbc57d6811ed47907df491
butterunderflow <azhong.934@gmail.com>
2024-02-10 02:07:22 +0800

    add visitor ppx to parsetree.ml and remove redundant "deriving" declaration

--- lib/syntax/parsetree.ml
+++ lib/syntax/parsetree.ml
@@ -1,6 +1,6 @@
 open Sexplib.Conv
 
-type program = top_level list [@@deriving sexp]
+type program = top_level list
 
 and top_level =
   | TopLet of pattern * expr
@@ -8,13 +8,11 @@ and top_level =
   | TopTypeDef of type_def
   | TopMod of string * mod_expr
   | TopModRec of (string * functor_expr) list
-[@@deriving sexp]
 
 and pattern =
   | PVal of constant
   | PCons of string * pattern list
   | PVar of string
-[@@deriving sexp]
 
 and type_expr =
   | TCons of string * type_expr list
@@ -23,20 +21,17 @@ and type_expr =
   | TArrow of type_expr * type_expr
   | TTuple of type_expr list
   | TRecord of (string * type_expr) list
-[@@deriving sexp, visitors { variety = "iter" }]
 
 and para =
   | PAnn of string * type_expr
   | PBare of string
-[@@deriving sexp]
 
-and paras = para list [@@deriving sexp]
+and paras = para list
 
 and constant =
   | CBool of bool
   | CInt of int
   | CString of string
-[@@deriving sexp]
 
 and expr =
   | EConst of constant
@@ -50,27 +45,24 @@ and expr =
   | EAnn of expr * type_expr
   | ETuple of expr list
   | EField of path * string
-[@@deriving sexp]
 
 and lambda = para * expr
 
-and variant = string * type_expr option [@@deriving sexp]
+and variant = string * type_expr option
 
-and type_paras = string list [@@deriving sexp]
+and type_paras = string list
 
 and type_def =
   | TDAdt of string * type_paras * variant list
   | TDAlias of string * type_expr
   | TDRecord of string * type_paras * (string * type_expr) list
-[@@deriving sexp]
 
-and mod_body = top_level list [@@deriving sexp]
+and mod_body = top_level list
 
 and path =
   | PName of string
   | PMem of path * string
   | PApply of path * path
-[@@deriving sexp]
 
 and mod_expr =
   | MEName of string
@@ -79,22 +71,22 @@ and mod_expr =
   | MEField of path * string
   | MEApply of mod_expr * mod_expr list
   | MERestrict of mod_expr * mod_type
-[@@deriving sexp]
 
-and functor_para = string * mod_type [@@deriving sexp]
+and functor_para = string * mod_type
 
-and functor_expr = functor_para * mod_expr [@@deriving sexp]
+and functor_expr = functor_para * mod_expr
 
 and mod_type =
   | MTName of string
   | MTField of path * string
   | MTSig of type_comp list
   | MTFunctor of string * mod_type * mod_type
-[@@deriving sexp]
 
-and adt_def = string * type_paras * variant list [@@deriving sexp]
+and adt_def = string * type_paras * variant list
 
 and type_comp =
   | TValueSpec of string * type_expr
   | TAbstTySpec of string
   | TManiTySpec of type_def
+[@@deriving
+  sexp, visitors { variety = "iter" }, visitors { variety = "map" }]
