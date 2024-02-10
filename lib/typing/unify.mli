8d341c1dd488367c8fa9e08fa2592384d398eabe
butterunderflow <azhong.934@gmail.com>
2024-02-10 17:45:23 +0800

    signature file of unify.ml

---
+++ lib/typing/unify.mli
@@ -0,0 +1,21 @@
+type t
+
+val apply : t -> Types.ty -> Types.ty
+
+val ( <$> ) : t -> Types.ty -> Types.ty
+
+val compose : t -> t -> t
+
+val identity : t
+
+val ( <.> ) : t -> t -> t
+
+val apply_lst : t -> Types.ty list -> Types.ty list
+
+val apply_env : t -> Env.t -> Env.t
+
+val unify : Types.ty -> Types.ty -> t
+
+val make_subst : string -> Types.ty -> t
+
+val make_subst_lst : string list -> Types.ty list -> t
