4c428cdac5f4f22333db58c214dbe37736c1dcc0
butterunderflow <azhong.934@gmail.com>
2024-07-10 22:44:56 +0800

    more runtime test

--- runtime/tests/test_runtime_operations.cpp
+++ runtime/tests/test_runtime_operations.cpp
@@ -36,12 +36,38 @@ ff_obj_t add_closure_cfn(ff_fvs_t fvs, ff_obj_t arg_z) {
     return ff_add_int(ff_add_int(fv_x, fv_y), arg_z);
 }
 
-TEST(FunRuntimeTest, ClosureOperation) {
+TEST(FunRuntimeTest, SimpleClosureCreation) {
     ff_obj_t fvs[] = {ff_make_int(1), ff_make_int(2)};
 
     auto clos = ff_make_closure(fvs, 2, (ff_erased_fptr)add_closure_cfn);
 
     ff_obj_t x = ff_make_int(3);
     auto result = ff_apply_generic(clos, x);
+    EXPECT_EQ(ff_get_int(result), 6);
+}
+
+
+// fun y ->  x + y + z
+ff_obj_t adder_closure_cfn1(ff_fvs_t fvs, ff_obj_t arg_y) {
+    auto fv_z = fvs[0];
+    auto fv_x = fvs[1];
+    return ff_add_int(ff_add_int(fv_x, arg_y), fv_z);
+}
+
+// fun arg_x -> fun y ->  x + y + z
+ff_obj_t adder_closure_cfn2(ff_fvs_t fvs, ff_obj_t arg_x) {
+    auto fv_z = fvs[0];
+    return ff_make_closure({fv_z, arg_x}, 2,
+                           (ff_erased_fptr)adder_closure_cfn1);
+}
+
+TEST(FunRuntimeTest, AdderClosureCreation) {
+    auto fv1 = ff_make_int(1);
+
+    auto clos1 = ff_make_closure({fv1}, 1, (ff_erased_fptr)adder_closure_cfn2);
+
+    auto clos2 = ff_apply_generic(clos1, ff_make_int(2));
+
+    auto result = ff_apply_generic(clos2, ff_make_int(3));
     EXPECT_EQ(ff_get_int(result), 6);
 }
