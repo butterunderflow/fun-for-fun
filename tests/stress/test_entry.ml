f11501e63b1468c88d4a3d0e5f5c558b814aff90
butterunderflow <azhong.934@gmail.com>
2024-07-14 23:07:14 +0800

    specify bound in fib.exe property

--- tests/stress/test_entry.ml
+++ tests/stress/test_entry.ml
@@ -2,7 +2,7 @@ module U = Util
 
 let sum_test =
   let rec sum x = if x = 0 then 0 else x + sum (x - 1) in
-  QCheck.Test.make ~count:10 ~name:"ff sum is correct" QCheck.small_nat
+  QCheck.Test.make ~count:10 ~name:"sum.exe property" QCheck.small_nat
     (fun i -> U.call_ff_int_to_int ~case:"sum.fun" i = sum i)
 
 let fib_test =
@@ -12,7 +12,7 @@ let fib_test =
     | 1 -> 1
     | _ -> fib (x - 1) + fib (x - 2)
   in
-  QCheck.Test.make ~count:3 ~name:"ff fib is correct" ~long_factor:5 QCheck.small_nat
+  QCheck.Test.make ~count:5 ~name:"fib.exe propety" (QCheck.int_bound 30)
     (fun i -> U.call_ff_int_to_int ~case:"fib.fun" i = fib i)
 
 let () = ignore (QCheck_runner.run_tests ~verbose:true [ sum_test; fib_test ])
