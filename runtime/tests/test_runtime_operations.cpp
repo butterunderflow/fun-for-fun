41459cb967db4c09c7edf07159c8390c46f101a0
butterunderflow <azhong.934@gmail.com>
2024-07-27 15:17:54 +0800

    some runtime test

--- runtime/tests/test_runtime_operations.cpp
+++ runtime/tests/test_runtime_operations.cpp
@@ -1,4 +1,5 @@
 #include "fun_rt.hpp"
+#include "fun_rt_core.hpp"
 #include <cstdint>
 #include <gtest/gtest.h>
 #include <memory>
@@ -15,10 +16,9 @@ TEST(FunRuntimeTest, BasicDataCreation) {
     auto z = ff_make_str("Hello");
     EXPECT_EQ(z.tag, FF_STR_TAG);
     EXPECT_STREQ(ff_get_str(z), "Hello");
-
-    z = ff_make_str("");
-    EXPECT_EQ(z.tag, FF_STR_TAG);
-    EXPECT_STREQ(ff_get_str(z), "");
+    auto z_1 = ff_make_str("");
+    EXPECT_EQ(z_1.tag, FF_STR_TAG);
+    EXPECT_STREQ(ff_get_str(z_1), "");
 }
 
 TEST(FunRuntimeTest, BasicDataOperation) {
@@ -77,4 +77,54 @@ TEST(FunRuntimeTest, BuiltinAdder) {
 
     EXPECT_EQ(ff_get_tag(result), FF_INT_TAG);
     EXPECT_EQ(ff_get_int(result), 15);
+}
+
+TEST(FunRuntimeTest, TupleCreation) {
+    auto x = ff_make_int(1);
+    auto y = ff_make_str("Hello");
+    auto z = ff_make_int(3);
+    auto tu = ff_make_tuple({x, y, z}, 3);
+
+    EXPECT_EQ(tu.tag, FF_TUPLE_TAG);
+}
+
+TEST(FunRuntimeTest, TupleMatch) {
+    auto x = ff_make_int(1);
+    auto y = ff_make_str("Hello");
+    auto z = ff_make_int(3);
+    auto tu = ff_make_tuple({x, y, z}, 3);
+
+    ff_obj_t x_1;
+    ff_obj_t y_1;
+    ff_obj_t z_1;
+    ff_match_tuple(tu, {&x_1, &y_1, &z_1});
+    EXPECT_EQ(x_1.tag, FF_INT_TAG);
+    EXPECT_TRUE(ff_is_equal_aux(x, x_1));
+
+    EXPECT_EQ(y_1.tag, FF_STR_TAG);
+    EXPECT_TRUE(ff_is_equal_aux(y, y_1));
+    EXPECT_EQ(z_1.tag, FF_INT_TAG);
+    EXPECT_TRUE(ff_is_equal_aux(z, z_1));
+}
+
+TEST(FunRuntimeTest, ModObjectCreation) {
+    {
+        auto x = ff_make_int(1);
+        auto y = ff_make_str("Hello");
+        auto z = ff_make_int(3);
+
+        auto mod = ff_make_mod_obj(3, {"x", "y", "z"}, {x, y, z});
+        EXPECT_EQ(mod.tag, FF_MODOBJ_TAG);
+
+        auto x_1 = ff_get_member(mod, "x");
+        EXPECT_TRUE(ff_is_equal_aux(x, x_1));
+
+        auto y_1 = ff_get_member(mod, "y");
+        EXPECT_TRUE(ff_is_equal_aux(y, y_1));
+    }
+
+    {
+        auto mod = ff_make_mod_obj(0, {}, {});
+        EXPECT_EQ(mod.tag, FF_MODOBJ_TAG);
+    }
 }
