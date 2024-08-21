ce081a6e2cbc8879cbbdbf33578fbcf70c59335a
butterunderflow <azhong.934@gmail.com>
2024-08-22 01:21:40 +0800

    re-organize error report mechanism

--- lib/typing/report.ml
+++ lib/typing/report.ml
@@ -1,29 +1,23 @@
 module T = Types_in
 module PP = Render.NoTypeHintPP
 
-type err =
-  | UnificationError of (T.ty * T.ty * Lexing.position * Lexing.position)
+exception UnificationError of (T.ty * T.ty)
 
-(* error reportings *)
-(* todo: support error tolerable type checking *)
-exception
-  UnificationError of T.ty * T.ty * Lexing.position * Lexing.position * Env.t
-
-let unification_error t0 t1 loc1 loc2 env =
-  raise (UnificationError (t0, t1, loc1, loc2, env))
+exception OccurError of string * T.ty
 
 exception ComponentInCompatible of string * T.bind_ty * T.bind_ty
 
-let in_compatible_error name t0 t1 =
-  raise (ComponentInCompatible (name, t0, t1))
+exception LocatedErr of exn * Lexing.position * Lexing.position * Env.t
 
-exception OccurError of string * T.ty * Lexing.position * Lexing.position
+let error_incompatible name vt0 vt1 =
+  raise (ComponentInCompatible (name, vt0, vt1))
 
-let occur_error tv te loc1 loc2 =
-  match !tv with
-  | T.Unbound (v, _lvl) ->
-      raise (OccurError (Ident.to_string v, te, loc1, loc2))
-  | T.Link _ -> failwith "neverreach"
+let error_unification t0 t1 = raise (UnificationError (t0, t1))
+
+let error_occur name te = raise (OccurError (Ident.to_string name, te))
+
+let wrap_and_reraise (kind : exn) start last env =
+  raise (LocatedErr (kind, start, last, env))
 
 let print_code_range (start : Lexing.position) (last : Lexing.position) =
   let start_col = start.pos_cnum - start.pos_bol in
@@ -33,25 +27,37 @@ let print_code_range (start : Lexing.position) (last : Lexing.position) =
 
 let unknown_location () = Printf.printf "At some unknown location: "
 
-let wrap_with_error_report f =
-  try Some (f ()) with
-  | UnificationError (t0, t1, start, last, env) ->
-      print_code_range start last;
-      Printf.printf "Can't unify `%s` with `%s`" (PP.pp_str_of_ty ~env t0)
-        (PP.pp_str_of_ty ~env t1);
+let report_error ?env (err : exn) =
+  match err with
+  | UnificationError (t0, t1) ->
+      Printf.printf "Can't unify `%s` with `%s`" (PP.pp_str_of_ty ?env t0)
+        (PP.pp_str_of_ty ?env t1);
       None
-  | OccurError (tv, te, start, last) ->
-      print_code_range start last;
+  | OccurError (tvn, te) ->
       Printf.printf "internal error: occur check error\n";
-      Printf.printf "type variable %s occured in " tv;
+      Printf.printf "type variable %s occured in " tvn;
       PP.pp_ty Format.std_formatter te;
       None
   | ComponentInCompatible (name, (_, t0), (_, t1)) ->
-      unknown_location ();
       Printf.printf
         "Value component %s has type `%s`, which is not compatible with `%s`"
         name (PP.pp_str_of_ty t0) (PP.pp_str_of_ty t1);
       None
   | Failure msg ->
       Printf.printf "%s\n" msg;
+      None
+  | _ ->
+      Printf.printf "unknown error";
+      None
+
+let wrap_with_error_report f =
+  try Some (f ()) with
+  | LocatedErr (e, start, last, env) ->
+      print_code_range start last;
+      report_error ~env e
+  | Failure msg ->
+      Printf.printf "%s\n" msg;
+      None
+  | _ ->
+      Printf.printf "Some unknown erroer occured";
       None
--- lib/typing/unify.mli
+++ lib/typing/unify.mli
@@ -2,6 +2,4 @@ val unify : Types_in.ty -> Types_in.ty -> unit
 
 val occur : Types_in.tv ref -> Types_in.ty -> bool
 
-exception UnificationError of (Types_in.ty * Types_in.ty)
-
 exception OccurError of (Types_in.tv ref * Types_in.ty)
